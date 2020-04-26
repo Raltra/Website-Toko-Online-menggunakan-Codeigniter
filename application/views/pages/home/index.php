d55133c9af6d6dbb1982f1612512d2424f6381ca
nardiyansah <janatri1398@gmail.com>
2020-04-26 22:47:23 +0700

    menggabungkan template pada project

---
+++ application/views/pages/home/index.php
@@ -0,0 +1,206 @@
+
+
+    <main role="main" class="container">
+      <div class="row">
+        <div class="col-md-9">
+          <div class="row">
+            <div class="col-md-12">
+              <div class="card mb-3">
+                <div class="card-body">
+                  Kategori : <strong>Semua Kategori</strong>
+                  <span class="float-right">
+                    Urutkan Harga :
+                    <a href="#" class="badge badge-primary">Termuah</a> |
+                    <a href="#" class="badge badge-primary">Termahal</a>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-6">
+              <div class="card mb-3">
+                <img
+                  src="https://placehold.co/100x70"
+                  alt=""
+                  class="card-img-top"
+                />
+                <div class="card-body">
+                  <h5 class="card-title">Product title</h5>
+                  <p class="card-text"><strong>Rp100.000,00</strong></p>
+                  <p class="card-text">Some text describe product here</p>
+                  <span class="badge badge-primary"
+                    ><i class="fas fa-tags"></i> Category</span
+                  >
+                  <div class="card-footer">
+                    <form action="">
+                      <div class="input-group mb-3">
+                        <input type="number" class="form-control" />
+                        <div class="input-group-append">
+                          <button
+                            class="btn btn-outline-secondary"
+                            type="submit"
+                            id="button-addon2"
+                          >
+                            Add to cart
+                          </button>
+                        </div>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="card mb-3">
+                <img
+                  src="https://placehold.co/100x70"
+                  alt=""
+                  class="card-img-top"
+                />
+                <div class="card-body">
+                  <h5 class="card-title">Product title</h5>
+                  <p class="card-text"><strong>Rp100.000,00</strong></p>
+                  <p class="card-text">Some text describe product here</p>
+                  <span class="badge badge-primary"
+                    ><i class="fas fa-tags"></i> Category</span
+                  >
+                  <div class="card-footer">
+                    <form action="">
+                      <div class="input-group mb-3">
+                        <input type="number" class="form-control" />
+                        <div class="input-group-append">
+                          <button
+                            class="btn btn-outline-secondary"
+                            type="submit"
+                            id="button-addon2"
+                          >
+                            Add to cart
+                          </button>
+                        </div>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="card mb-3">
+                <img
+                  src="https://placehold.co/100x70"
+                  alt=""
+                  class="card-img-top"
+                />
+                <div class="card-body">
+                  <h5 class="card-title">Product title</h5>
+                  <p class="card-text"><strong>Rp100.000,00</strong></p>
+                  <p class="card-text">Some text describe product here</p>
+                  <span class="badge badge-primary"
+                    ><i class="fas fa-tags"></i> Category</span
+                  >
+                  <div class="card-footer">
+                    <form action="">
+                      <div class="input-group mb-3">
+                        <input type="number" class="form-control" />
+                        <div class="input-group-append">
+                          <button
+                            class="btn btn-outline-secondary"
+                            type="submit"
+                            id="button-addon2"
+                          >
+                            Add to cart
+                          </button>
+                        </div>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="card mb-3">
+                <img
+                  src="https://placehold.co/100x70"
+                  alt=""
+                  class="card-img-top"
+                />
+                <div class="card-body">
+                  <h5 class="card-title">Product title</h5>
+                  <p class="card-text"><strong>Rp100.000,00</strong></p>
+                  <p class="card-text">Some text describe product here</p>
+                  <span class="badge badge-primary"
+                    ><i class="fas fa-tags"></i> Category</span
+                  >
+                  <div class="card-footer">
+                    <form action="">
+                      <div class="input-group mb-3">
+                        <input type="number" class="form-control" />
+                        <div class="input-group-append">
+                          <button
+                            class="btn btn-outline-secondary"
+                            type="submit"
+                            id="button-addon2"
+                          >
+                            Add to cart
+                          </button>
+                        </div>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <nav aria-label="Page navigation example">
+            <ul class="pagination">
+              <li class="page-item">
+                <a class="page-link" href="#">Previous</a>
+              </li>
+              <li class="page-item"><a class="page-link" href="#">1</a></li>
+              <li class="page-item"><a class="page-link" href="#">2</a></li>
+              <li class="page-item"><a class="page-link" href="#">3</a></li>
+              <li class="page-item"><a class="page-link" href="#">Next</a></li>
+            </ul>
+          </nav>
+        </div>
+        <div class="col-md-3">
+          <div class="row mb-3">
+            <div class="col-md-12">
+              <div class="card">
+                <div class="card-header">Pencarian</div>
+                <div class="card-body">
+                  <form action="">
+                    <div class="input-group mb-3">
+                      <input type="text" class="form-control" />
+                      <div class="input-group-append">
+                        <button
+                          class="btn btn-outline-secondary"
+                          type="submit"
+                          id="button-addon2"
+                        >
+                          Cari
+                        </button>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-12">
+              <div class="card">
+                <div class="card-header">Kategori</div>
+                <ul class="list-group">
+                  <li class="list-group-item">Semua Kategori</li>
+                  <li class="list-group-item">Kategori 1</li>
+                  <li class="list-group-item">Kategori 2</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+    
