18556d860c07f4eae00b5a0230db169b1aa8f70e
nardiyansah <janatri1398@gmail.com>
2020-04-27 17:04:14 +0700

    solve problem unable to locate the model you have specified

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -1,29 +1,28 @@
 <?php
 
-
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class MY_Controller extends CI_Controller {
+class MY_Controller extends CI_Controller 
+{
 
-    
-    public function __construct()
-    {
-        parent::__construct();
-        $model = strtolower(get_class($this));
-        if(file_exists(APPPATH . 'models/' . $model . '_model.php')){
-            $this->load->model($model . '_model.php', $model, true);
-        }
-    }
-    
+	public function __construct()
+	{
+		parent::__construct();
+		$model = strtolower(get_class($this));
+		if (file_exists(APPPATH . 'models/'. ucfirst($model) . '_model.php')) {
+			$this->load->model(ucfirst($model) . '_model', $model, true);
+		}
+	}
+	
+	/**
+	 * Load view with default layouts
+	 *
+	 * @param [type] $data
+	 * @return void
+	 */
+	public function view($data)
+	{
+		$this->load->view('layouts/app', $data);
+	}
 
-    /***
-     * load view with default layout
-     */
-
-     public function view($data){
-         $this->load->view('layouts/app', $data);
-     }
-     
 }
-
-/* End of file MY_Controller.php */
---
+++ application/views/pages/auth/register.php
@@ -0,0 +1,44 @@
+<main role="main" class="container">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 mx-auto">
+            <div class="card">
+              <div class="card-header">Register</div>
+              <div class="card-body">
+                <form action="">
+                  <div class="form-group">
+                    <label>Name</label>
+                    <input type="text" class="form-control" name="" />
+                    <small class="form-text text-danger"
+                      >Nama harus diisi</small
+                    >
+                  </div>
+                  <div class="form-group">
+                    <label>E-mail</label>
+                    <input type="email" class="form-control" name="" />
+                    <small class="form-text text-danger"
+                      >E-mail harus diisi</small
+                    >
+                  </div>
+                  <div class="form-group">
+                    <label>Password</label>
+                    <input type="password" class="form-control" name="" />
+                    <small class="form-text text-danger"
+                      >Password harus diisi</small
+                    >
+                  </div>
+                  <div class="form-group">
+                    <label>Retype Password</label>
+                    <input type="password" class="form-control" name="" />
+                    <small class="form-text text-danger"
+                      >Password harus sama</small
+                    >
+                  </div>
+                  <button type="submit" class="btn btn-primary">Login</button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
